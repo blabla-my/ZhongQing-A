a96e942c58ae5de371aed95bb4945f140648d4b6
blabla-my <1042244638@qq.com>
2021-05-22 14:09:13 +0800

    add task graph operation

--- task.py
+++ task.py
@@ -1,5 +1,6 @@
 from node import *
 import itertools
+import copy
 class task :
     def __init__(self, area = 0, number = 0, sequence_number = 0, time = 0):
         self.dest = nodes[idx(area,number)]
@@ -32,6 +33,47 @@ class task :
     def __eq__(self,another):
         return self.time == another.time
 
+class task_graph :
+    def __init__(self,tasks):
+        self.tasks = copy.deepcopy(tasks)
+        list.sort(self.tasks)
+        self.entry = list()
+        for i in range(0,len(self.tasks)):
+            for j in range (i+1,len(self.tasks)):
+                if self.tasks[i].able_to_reach(self.tasks[j]):
+                    self.tasks[i].linkto(self.tasks[j])
+            if len(self.tasks[i].parent) == 0:
+                self.entry.append(self.tasks[i])
+        list.sort(self.entry)
+    
+    def get_task(self,number):
+        for t in self.tasks:
+            if number == t.number :
+                return t
+        return None
+
+    def show(self):
+        for t in self.tasks:
+            print('{} --> {}'.format(t.number,list(map(lambda x: x.number ,t.childs))))
+        print("entry: {}".format(list(map(lambda x: x.number ,self.entry))))
+
+    def remove(self,_task):
+        if _task in self.tasks:
+            self.tasks.remove(_task)
+        else:
+            print("not in graph")
+            return
+        for next in _task.childs :
+            next.parent.remove(_task)
+        for pare in _task.parent :
+            pare.childs.remove(_task)
+        if _task in self.entry :
+            self.entry.remove(_task)
+    
+    
+        
+
+
 def create_graph(tasks):
     list.sort(tasks)
     for i in range(0,len(tasks)):
@@ -52,6 +94,7 @@ def map_dataline_to_task(line):
 def show_graph(g):
     for t in g:
         print('{} --> {}'.format(t.number,list(map(lambda x: x.number ,t.childs))))
+    
 
 def fetch_sequence_time(feseq):
     assert(len(feseq) <= 4)
@@ -77,15 +120,14 @@ def min_fetch_sequence_time(feseq):
     
         
 tasks = []
-with open('tasks2.txt','r') as f:
+with open('tasks1.txt','r') as f:
     Lines = f.readlines()
     for line in Lines:
         tasks.append(map_dataline_to_task(line))
 
-print(len(tasks))
 m, per_res = min_fetch_sequence_time(tasks[:4])
 
-show_graph(per_res)
-
-#create_graph(tasks)
-#show_graph(tasks)
+g = task_graph(tasks)
+g.show()
+g.remove(g.get_task(1))
+g.show()
